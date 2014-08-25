ed6c4b0a15a1fc20cfa59eddc8f37718c6eb275a
Richard Rothwell <rrothwell@v3.org.au>
2014-08-25 13:12:08 +1000

    RR: fixed tests for new email address used in fixtures.
    Change-Id: I43a9fc5cd1960e99848464f44abafb1bc2ff7475

--- nectar_allocations/tests/test_db_allocation.py
+++ nectar_allocations/tests/test_db_allocation.py
@@ -37,7 +37,7 @@ class AllocationDBTest(TestCase):
         sub_allocations.sort(key=lambda summary: summary['project_name'] + str(summary['id']))
       
         sub_allocation = sub_allocations[0]
-        self.assertEquals('usq.edu.au', sub_allocation['institution'])
+        self.assertEquals('qqqqqq.edu.au', sub_allocation['institution'])
         self.assertEquals('USQ eResearch Services Sandbox', sub_allocation['project_name'])
         self.assertEquals('099901', sub_allocation['for_6'])
         self.assertEquals('0999', sub_allocation['for_4'])
@@ -46,7 +46,7 @@ class AllocationDBTest(TestCase):
         self.assertAlmostEqual(2.4, sub_allocation['core_quota'], 2)
          
         sub_allocation = sub_allocations[1]
-        self.assertEquals('usq.edu.au', sub_allocation['institution'])
+        self.assertEquals('qqqqqq.edu.au', sub_allocation['institution'])
         self.assertEquals('USQ eResearch Services Sandbox', sub_allocation['project_name'])
         self.assertEquals('070104', sub_allocation['for_6'])
         self.assertEquals('0701', sub_allocation['for_4'])
@@ -55,7 +55,7 @@ class AllocationDBTest(TestCase):
         self.assertAlmostEqual(1.6, sub_allocation['core_quota'], 2)
        
         sub_allocation = sub_allocations[2]
-        self.assertEquals('unimelb.edu.au', sub_allocation['institution'])
+        self.assertEquals('gggg.edu.au', sub_allocation['institution'])
         self.assertEquals('UoM_Trajectory_Inference_Attacks', sub_allocation['project_name'])
         self.assertEquals('080109', sub_allocation['for_6'])
         self.assertEquals('0801', sub_allocation['for_4'])
