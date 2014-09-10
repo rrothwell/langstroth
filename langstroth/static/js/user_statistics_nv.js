cf4e006097a8185a4601a40620947eb66a41dae0
Richard Rothwell <rrothwell@v3.org.au>
2014-09-10 17:59:32 +1000

    RR: changed frequency graph to use a bar chart.
    Change-Id: I0418c481f971f3e4739b323eead581e246005cb8

--- langstroth/static/js/user_statistics_nv.js
+++ langstroth/static/js/user_statistics_nv.js
@@ -5,40 +5,72 @@ var dateFormat = d3.time.format("%Y-%m-%d");
 var parseDate = dateFormat.parse;
 var frequencyFormat = d3.format(',.0f');
 
-var svg = d3.select("#plot-area").append("svg");
+// Build area chart
 
-function visualise(trend) {
+var areaChart = nv.models.stackedAreaChart()
+	.margin({right: 100})
+	.x(function(d) { 
+		  return d[0].getTime(); 
+		  })   
+	.y(function(d) { 
+		  return d[1]; 
+		  })  
+	.useInteractiveGuideline(true) 
+	.rightAlignYAxis(true) 
+	.transitionDuration(500)
+	.showControls(false)
+	.clipEdge(true);
+	
+	areaChart.xAxis.tickFormat(function(d) {
+	return dateFormat(new Date(d)) ;
+	});
+
+	areaChart.yAxis.tickFormat(frequencyFormat);
+	
+// Build histogram chart
+	
+	var histoChart = nv.models.historicalBarChart()
+		.x(function(d, i) { 
+			return d[0].getTime(); 
+		})   
+		.y(function(d) { 
+			return d[1]; 
+		})  
+		.rightAlignYAxis(true) 
+		.transitionDuration(500)
+		.clipEdge(false);
+
+		histoChart.xAxis.tickFormat(function(d) {
+			return dateFormat(new Date(d)) ;
+		});
+
+		histoChart.yAxis.tickFormat(frequencyFormat);
+		histoChart.showLegend(true);
 		
-	  nv.addGraph(function() {
-		    var chart = nv.models.stackedAreaChart()
-		                  .margin({right: 100})
-		                  .x(function(d) { 
-		                	  return d[0].getTime(); 
-		                	  })   
-		                  .y(function(d) { 
-		                	  return d[1]; 
-		                	  })  
-		                  .useInteractiveGuideline(true) 
-		                  .rightAlignYAxis(true) 
-		                  .transitionDuration(500)
-		                  .showControls(false)
-		                  .clipEdge(true);
 
-		    chart.xAxis
-		    .tickFormat(function(d) {
-		      return dateFormat(new Date(d)) ;
-		    });
+// Render the chart
+		
+var svg = null;
 
-		    chart.yAxis
-		    .tickFormat(frequencyFormat);
+function visualise(trend, chart) {
 
-			svg.datum(trend).call(chart);	
+	// Clean up the internal graph array.
+	while(nv.graphs.length > 0) {
+		nv.graphs.pop();
+	}	
+	
+	// Freshen dom elements.
+	if (svg) {
+		svg.remove();
+	}
+	svg = d3.select("#plot-area").append("svg");
 
-		    nv.utils.windowResize(chart.update);
-
-		    return chart;
-		  });
-  
+	// Add the chosen graph
+	nv.addGraph(function() {
+		svg.datum(trend).call(chart);	
+		nv.utils.windowResize(chart.update);
+	    return chart;
+	}); 
 }
 
 function processResponse(registrationFrequency, legendKey) {
@@ -62,16 +94,28 @@ function processResponse(registrationFrequency, legendKey) {
 	return [wrappedTrend];
 }
 
+function expandXDomain(trend) {
+	var xExtent = d3.extent(trend[0].values, function(d) {
+		return d[0];
+	});
+	var lastDate = xExtent[1];
+	lastDate.setMonth(lastDate.getMonth() + 1);
+	histoChart.xDomain(xExtent);
+}
+
 function load() {
 	d3.json("/user_statistics/rest/registrations/frequency", function(error, responseData) {
 		registrationFrequency = responseData;
 		var legendKey = 'User Registrations At Month';
 		var trend = processResponse(registrationFrequency, legendKey);
-		visualise(trend);
+		expandXDomain(trend);
+		visualise(trend, areaChart);
 	});
 }
 
 load();
+
+// Flick between the 2 kinds of chart/data.
 
 function change() {
 	$('#graph-buttons button').removeClass('active');
@@ -79,7 +123,8 @@ function change() {
 	isCumulative = this.id == 'cumulative';
 	var legendKey = isCumulative ? 'User Registrations At Month' : 'User Registrations Per Month';
 	var trend = processResponse(registrationFrequency, legendKey);
-	visualise(trend);
+	var chart = isCumulative ? areaChart : histoChart;
+	visualise(trend, chart);
 }
 
 d3.selectAll("button").on("click", change);
